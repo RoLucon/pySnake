a94c9dac773d9930b33d472c1ac300e6099e61f0
Rogerio Lucon <rogerios.lucon@gmail.com>
2019-10-01 21:28:47 -0300

    Game Objects e Snake

---
+++ game_object.py
@@ -0,0 +1,21 @@
+class GameObject:
+
+    def __init__(self, pos, size, color):
+        self.pos_x = pos[0]
+        self.pos_y = pos[1]
+        self.width = size[0]
+        self.heigth = size[1]
+        self.color = color
+        self.pivot = (self.pos_x - self.width / 2, self.pos_y - self.heigth / 2)
+
+
+class Snake(GameObject):
+
+    def __init__(self, pos, size, color):
+        GameObject.__init__(self, pos, size, color)
+        self.velocity_x = 0
+        self.velocity_y = 0
+
+    def update(self):
+        self.pos_x += self.velocity_x
+        self.pos_y += self.velocity_y
